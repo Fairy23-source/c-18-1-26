af00c6e1b58982f13593af1f5d8e80f1dc64adbf
Fairy Solanki <solankifairy612@gmail.com>
2026-01-18 11:57:30 +0530

    Add files via upload

---
+++ 12a.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+//using concept of pointers in this program
+int main () {
+    int a=3;
+    int *b=&a;
+    cout<<b<<endl;//for finding add & is used
+    cout<<"the value at b is"<<*b<<endl;//to find value * is used
+    int **c=&b;//pointer to pointer 
+    cout<<&b<<endl;
+    cout<<**c<<endl;
+    return 0;
+}
