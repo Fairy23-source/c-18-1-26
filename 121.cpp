04178b3fd8d3f67265ceef35ed3a8aff1e255131
Fairy Solanki <solankifairy612@gmail.com>
2026-01-18 13:23:12 +0530

    Add files via upload

---
+++ 121.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int marks[5]={22,34,56,78,34};
+    cout<<marks[0]<<endl;
+    cout<<marks[1]<<endl;
+    marks[2]=45;//you can also change value of array
+    cout<<marks[2]<<endl;
+    cout<<marks[3]<<endl;
+    cout<<marks[4]<<endl;
+    return 0;
+    
+
+}
