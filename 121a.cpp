ab9f9a06052772bb652ab7452976937ba49369a3
Fairy Solanki <solankifairy612@gmail.com>
2026-01-18 13:26:35 +0530

    Add files via upload

---
+++ 121a.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int i;
+    int marks[5]={22,34,56,78,34};
+    for(i=0; i<=5;i++){
+        cout<<"marks is "<<marks[i]<<endl;
+    }
+    return 0;
+    
+
+}
